5daad2da98072318bcaea99831238781db717508
ctf0 <muah003@gmail.com>
2023-05-04 00:23:32 +0300

    002

--- src/providers/hoverProvider.ts
+++ src/providers/hoverProvider.ts
@@ -21,71 +21,99 @@ export default class HoverProvider implements vscode.HoverProvider {
         const separator = ' $(kebab-vertical) ';
 
         const currentLine = position.line;
-        const symbols: vscode.DocumentSymbol[] | undefined = filterSameLineSymbols(getAllSymbols(await getFileSymbols(uri)));
+        const symbol: any = await findSymbol(await getFileSymbols(uri), currentLine, uri);
 
-        if (symbols && symbols.length) {
-            for (const symbol of symbols) {
+        if (symbol) {
+            const start = symbol.range.start;
+            const end = symbol.range.end;
+            const selectionStart = symbol.selectionRange.start;
+            const selectionEnd = symbol.selectionRange.end;
+
+            const atStart = start.line === currentLine;
+            const atEnd = end.line === currentLine;
+            const atSelectionStart = selectionStart.line === currentLine;
+            const atSelectionEnd = selectionEnd.line === currentLine;
+
+            const link = new vscode.MarkdownString('', true);
+            link.isTrusted = true;
+
+            if (atStart) {
+                const argsJump = prepareArgs(new vscode.Range(end, end), 'down');
+                const jumpEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsJump}`);
+
+                const argsSelect = prepareArgs(new vscode.Range(start, end), 'down');
+                const selectEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsSelect}`);
+
+                link.appendMarkdown(`[${jump.text}${jump.icon.down}](${jumpEnd})`);
+                link.appendMarkdown(separator);
+                link.appendMarkdown(`[${select.text}${select.icon}](${selectEnd})`);
+
+                return new vscode.Hover(link, new vscode.Range(start, start));
+            }
+
+            if (atSelectionStart || atSelectionEnd) {
+                const argsJump = prepareArgs(new vscode.Range(end, end), 'down');
+                const jumpEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsJump}`);
+
+                const argsSelect = prepareArgs(new vscode.Range(selectionStart, end), 'down');
+                const selectEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsSelect}`);
+
+                link.appendMarkdown(`[${jump.text}${jump.icon.down}](${jumpEnd})`);
+                link.appendMarkdown(separator);
+                link.appendMarkdown(`[${select.text}${select.icon}](${selectEnd})`);
+
+                return new vscode.Hover(link, new vscode.Range(selectionStart, selectionStart));
+            }
+
+            if (atEnd) {
+                const argsJump = prepareArgs(new vscode.Range(start, start), 'up');
+                const jumpStart = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsJump}`);
+
+                const argsSelect = prepareArgs(new vscode.Range(end, start), 'up');
+                const selectStart = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsSelect}`);
+
+                link.appendMarkdown(`[${jump.text}${jump.icon.up}](${jumpStart})`);
+                link.appendMarkdown(separator);
+                link.appendMarkdown(`[${select.text}${select.icon}](${selectStart})`);
+
+                return new vscode.Hover(link, new vscode.Range(end, end));
+            }
+        }
+    }
+}
+
+function findSymbol(symbols: vscode.DocumentSymbol[] | undefined, currentLine: number, uri: vscode.Uri) {
+    return new Promise(async (resolve, reject) => {
+        const symbolsList = filterSameLineSymbols(symbols);
+
+        if (symbolsList && symbolsList.length) {
+            for (const symbol of symbolsList) {
                 const start = symbol.range.start;
                 const end = symbol.range.end;
-                const atStart = start.line === currentLine;
-                const atEnd = end.line === currentLine;
-
                 const selectionStart = symbol.selectionRange.start;
                 const selectionEnd = symbol.selectionRange.end;
+
+                const atStart = start.line === currentLine;
+                const atEnd = end.line === currentLine;
                 const atSelectionStart = selectionStart.line === currentLine;
                 const atSelectionEnd = selectionEnd.line === currentLine;
 
                 if (atStart || atEnd || atSelectionStart || atSelectionEnd) {
-                    const link = new vscode.MarkdownString('', true);
-                    link.isTrusted = true;
+                    resolve(symbol);
+                    break;
+                }
 
-                    if (atStart) {
-                        const argsJump = prepareArgs(new vscode.Range(end, end), 'down');
-                        const jumpEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsJump}`);
-
-                        const argsSelect = prepareArgs(new vscode.Range(start, end), 'down');
-                        const selectEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsSelect}`);
-
-                        link.appendMarkdown(`[${jump.text}${jump.icon.down}](${jumpEnd})`);
-                        link.appendMarkdown(separator);
-                        link.appendMarkdown(`[${select.text}${select.icon}](${selectEnd})`);
-
-                        return new vscode.Hover(link, new vscode.Range(start, start));
-                    }
-
-                    if (atSelectionStart) {
-                        const argsJump = prepareArgs(new vscode.Range(end, end), 'down');
-                        const jumpEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsJump}`);
-
-                        const argsSelect = prepareArgs(new vscode.Range(selectionStart, end), 'down');
-                        const selectEnd = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsSelect}`);
-
-                        link.appendMarkdown(`[${jump.text}${jump.icon.down}](${jumpEnd})`);
-                        link.appendMarkdown(separator);
-                        link.appendMarkdown(`[${select.text}${select.icon}](${selectEnd})`);
-
-                        return new vscode.Hover(link, new vscode.Range(selectionStart, selectionStart));
-                    }
-
-                    if (atEnd) {
-                        const argsJump = prepareArgs(new vscode.Range(start, start), 'up');
-                        const jumpStart = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsJump}`);
-
-                        const argsSelect = prepareArgs(new vscode.Range(end, start), 'up');
-                        const selectStart = vscode.Uri.parse(`command:scopeJump.jumpToLine?${argsSelect}`);
-
-                        link.appendMarkdown(`[${jump.text}${jump.icon.up}](${jumpStart})`);
-                        link.appendMarkdown(separator);
-                        link.appendMarkdown(`[${select.text}${select.icon}](${selectStart})`);
-
-                        return new vscode.Hover(link, new vscode.Range(end, end));
-                    }
-                } else {
-                    continue;
+                if (symbol.children) {
+                    try {
+                        resolve(await findSymbol(symbol.children, currentLine, uri));
+                        break;
+                    } catch (error) { }
                 }
             }
         }
-    }
+
+        reject(undefined);
+    });
 }
 
 function prepareArgs(range: vscode.Range, to: string): string {
@@ -99,21 +127,6 @@ async function getFileSymbols(uri: vscode.Uri): Promise<vscode.DocumentSymbol[] 
     return vscode.commands.executeCommand('vscode.executeDocumentSymbolProvider', uri);
 }
 
-function getAllSymbols(symbols: vscode.DocumentSymbol[] | undefined): vscode.DocumentSymbol[] {
-    const list: vscode.DocumentSymbol[] = [];
-
-    if (symbols && symbols.length) {
-        for (const symbol of symbols) {
-            list.push(symbol);
-
-            if (symbol.children.length) {
-                list.push(...getAllSymbols(symbol.children));
-            }
-        }
-    }
-
-    return list;
-}
 
 function filterSameLineSymbols(symbols: vscode.DocumentSymbol[] | undefined): vscode.DocumentSymbol[] | undefined {
     return symbols?.filter((symbol: vscode.DocumentSymbol) => symbol.range.isSingleLine === false);
